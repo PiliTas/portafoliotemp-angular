75e82e7be68ae5995bea87412c2697d48f914d4b
NPGonzalez <naypili@gmail.com>
2020-05-28 23:23:39 -0400

    Se agrega constante que recupera el año actual en footer.component.ts

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+
+  anio: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
 
 }
